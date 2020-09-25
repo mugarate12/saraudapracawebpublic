afe901c0acab40f0e41ea9d70338a688dcd00f08
unknown <serjumano17@gmail.com>
2020-09-25 00:09:14 -0300

    first commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import GlobalStyle from './../../styles/global'
+
+export default function index() {
+  return (
+    <>
+      <GlobalStyle />
+      
+      <h1>OLÁ</h1>
+    </>
+  )
+}
