6e8b3e8a463d460b034bdcf5a8fcd4ec0932b2f2
mugarate12 <serjumano17@gmail.com>
2020-09-28 16:31:04 -0300

    adding comments to changes to move a pen in PaperAndPen Component

--- src/components/PaperAndPen/index.tsx
+++ src/components/PaperAndPen/index.tsx
@@ -13,12 +13,13 @@ const PaperAndPen: React.FC<PaperAndPenInterface> = ({ linelenght, numberline, l
   function movePenInPaper() {
     let top = -56
     let left = 38
-    let topNextLine = 12
+    let topNextLine = 11
     let topIncrement = -1
     let leftIncrement = 6
     let lineLimit = 14
     let rotationPen
-
+    
+    // mudar a regra de como muda a caneta de linha
     if (numberline === 1) {
       let topAlteration
       let leftAlteration
@@ -62,22 +63,37 @@ const PaperAndPen: React.FC<PaperAndPenInterface> = ({ linelenght, numberline, l
     }
   }
 
+  // 14 letras por linha
+  function renderLinesInPaper() {
+    // preciso pegar um estado, verificar seu tamanho e se passar de .length == 14 dividir esse conteudo
+    // será dividido em 14 letras por linha
+    // no máximo duas linhas por estado
+
+    return (
+      <>
+        <Styled.paperLine >
+          <Styled.PaperLineContent>{lineOneContent}</Styled.PaperLineContent>
+        </Styled.paperLine>
+        
+        <Styled.paperLine >
+          <Styled.PaperLineContent>{lineTwoContent}</Styled.PaperLineContent>
+        </Styled.paperLine>
+
+        <Styled.paperLine />
+        <Styled.paperLine />
+        <Styled.paperLine />
+        <Styled.paperLine />
+        <Styled.paperLine />
+        <Styled.paperLine />
+      </>
+    )
+  }
+
   return (
     <Styled.ContainerG>
       <Styled.Container>
         <Styled.paperContainer>
-          <Styled.paperLine >
-            <Styled.PaperLineContent>{lineOneContent}</Styled.PaperLineContent>
-          </Styled.paperLine>
-          <Styled.paperLine >
-            <Styled.PaperLineContent>{lineTwoContent}</Styled.PaperLineContent>
-          </Styled.paperLine>
-          <Styled.paperLine />
-          <Styled.paperLine />
-          <Styled.paperLine />
-          <Styled.paperLine />
-          <Styled.paperLine />
-          <Styled.paperLine />
+          {renderLinesInPaper()}
         </Styled.paperContainer>
 
         <Styled.penContainer style={movePenInPaper()}>
